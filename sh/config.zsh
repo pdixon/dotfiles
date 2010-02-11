0cab874ec397bb0e66855541f4d82171b3ba58e0
Phillip DIXON <pdixon@dynamiccontrols.com>
2010-02-12 09:12:59 +1300

    color for zsh prompt

--- sh/config.zsh
+++ sh/config.zsh
@@ -4,14 +4,14 @@ setopt prompt_subst
 
 function hg_prompt_info {
     hg prompt --angle-brackets "\
-< on <branch>>\
-< at <tags|, >>\
-<status|modified|unknown><update><
-patches: <patches|join( → )>>" 2>/dev/null
+< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
+%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
+patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
 }
 
 PROMPT='
-%~$(hg_prompt_info)
+%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}%~%{$reset_color%}$(hg_prompt_info)
 → %{$reset_color%}'
 
 # Set RPROMPT to have info show up on the right, too.
