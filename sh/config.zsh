f85c2427486b8d537ba29e51de1da8149834a7f5
Phillip DIXON <phil@dixon.gen.nz>
2010-06-03 21:41:36 +1200

    shell titles in screen from zsh

--- sh/config.zsh
+++ sh/config.zsh
@@ -53,6 +53,30 @@ PROMPT='
 
 # Set RPROMPT to have info show up on the right, too.
 
+# Set the titles in terminals.
+setopt extended_glob
+case "$TERM" in
+  xterm*|rxvt*)
+    preexec () {
+      print -Pn "\e]0;%n@%m: $1\a"  # xterm
+    }
+    precmd () {
+      print -Pn "\e]0;%n@%m: %~\a"  # xterm
+    }
+    ;;
+  screen*)
+    preexec () {
+      local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
+      echo -ne "\ek$CMD\e\\"
+ #     print -Pn "\e]0;%n@%m: $1\a"  # xterm
+    }
+    precmd () {
+      echo -ne "\ekzsh\e\\"
+#      print -Pn "\e]0;%n@%m: %~\a"  # xterm
+    }
+    ;;
+esac
+
 #############################
 # Other Options
 
