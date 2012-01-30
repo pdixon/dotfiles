e8c04e39bca8eaa3107b340cae700cd2becb4d77
Phillip DIXON <phil@dixon.gen.nz>
2012-01-30 18:47:23 +1300

    New git branch prompt.

--- sh/config.zsh
+++ sh/config.zsh
@@ -1,146 +1,25 @@
+# Use nice names for colors
 autoload -U colors
 colors
 setopt prompt_subst
 
-# get the name of the branch we are on
-function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+function git_current_branch() {
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+    echo "${ref#refs/heads/}"
 }
 
-parse_git_dirty () {
-  gitstat=$(git status 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
-
-  if [[ $(echo ${gitstat} | grep -c "^# Changes to be committed:$") > 0 ]]; then
-	echo -n "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  fi
-
-  if [[ $(echo ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\)$") > 0 ]]; then
-	echo -n "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
-  fi 
-
-  if [[ $(echo ${gitstat} | grep -v '^$' | wc -l | tr -d ' ') == 0 ]]; then
-	echo -n "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  fi
+function git_commit_id() {
+    echo "$(git rev-parse --short HEAD 2>/dev/null)"
 }
 
-#
-# Will return the current branch name
-# Usage example: git pull origin $(current_branch)
-#
-function current_branch() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo ${ref#refs/heads/}
-}
-
-ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-
-function hg_prompt_info {
-    hg prompt --angle-brackets "\
-< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
-< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
-%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
-patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
+function git_repo_clean() {
+    if [[ -n $(git status --short 2> /dev/null) ]]; then
+        echo "✗"
+    fi
 }
 
 PROMPT='
-%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}%~%{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
-→ %{$reset_color%}'
+%~
+→ '
 
-# Set RPROMPT to have info show up on the right, too.
-
-# Set the titles in terminals.
-setopt extended_glob
-case "$TERM" in
-  xterm*|rxvt*)
-    preexec () {
-      print -Pn "\e]0;%n@%m: $1\a"  # xterm
-    }
-    precmd () {
-      print -Pn "\e]0;%n@%m: %~\a"  # xterm
-    }
-    ;;
-  screen*)
-    preexec () {
-      local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
-      echo -ne "\ek$CMD\e\\"
- #     print -Pn "\e]0;%n@%m: $1\a"  # xterm
-    }
-    precmd () {
-      echo -ne "\ekzsh\e\\"
-#      print -Pn "\e]0;%n@%m: %~\a"  # xterm
-    }
-    ;;
-esac
-
-#############################
-# Other Options
-
-# setopt PRINT_EXIT_VALUE
-
-setopt CORRECT
-setopt CORRECTALL
-
-setopt hist_ignore_dups     # ignore duplication command history list
-setopt share_history        # share command history data
-setopt HIST_IGNORE_SPACE
-setopt APPEND_HISTORY # write history only when closing
-setopt EXTENDED_HISTORY # add more info
-
-# Other tabbing options
-# setopt NO_AUTO_MENU
-# setopt BASH_AUTO_LIST
-
-#############################
-# Variables
-
-# Quote pasted URLs
-autoload url-quote-magic
-zle -N self-insert url-quote-magic
-
-HISTFILE=~/.zsh_history
-SAVEHIST=10000
-HISTSIZE=10000
-
-REPORTTIME=10 # Show elapsed time if command took more than X seconds
-LISTMAX=0 # ask to complete if top of list would scroll off screen
-
-# Load completions for Ruby, Git, etc.
-autoload compinit
-compinit
-
-setopt autopushd
-
-
-# autoload -U zsh-mime-setup
-# zsh-mime-setup
-
-compdef '_files -g "*.mdwn"' mdwn2html
-compdef '_files -g "*.mdwn"' mdwn2odt
-compdef '_files -g "*.mdwn"' markdown2pdf
-
-
-# Line Editor Setup.
-
-bindkey -e
-
-# Make CTRL-W delete after other chars, not just spaces
-WORDCHARS=${WORDCHARS//[&=\/;\!#%\{]}
-
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '^Xe' edit-command-line
-
-autoload -U up-line-or-beginning-search
-zle -N up-line-or-beginning-search
-bindkey '^[[A' up-line-or-beginning-search
-
-autoload -U down-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey '^[[B' down-line-or-beginning-search
-
- 
+RPROMPT='%{$fg[white]%} $(git_current_branch) $(git_commit_id) $(git_repo_clean)%{$reset_color%}'
