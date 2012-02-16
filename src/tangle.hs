fc36dad20f710611a4f1739bd33250c470518be2
Phillip DIXON <phil@dixon.gen.nz>
2012-02-16 23:15:38 +1300

    Tangle works. I can dump files from a markdown document.

--- src/tangle.hs
+++ src/tangle.hs
@@ -2,23 +2,32 @@ import Text.Pandoc
 import Text.JSON.Generic
 import Data.Map as M hiding (lookup)
 
-
 extractCodeblock :: Block -> [(String, String)]
 extractCodeblock cb@(CodeBlock (id, classes, namevals) contents) =
     case lookup "filename" namevals of
       Just f -> [(f, contents)]
-      Nothing -> [("","")]
-extractCodeblock x = [("","")]
+      Nothing -> []
+extractCodeblock x = []
 
 extractCodeblocks :: Pandoc -> [(String, String)]
 extractCodeblocks = queryWith extractCodeblock
 
-mergeWithNewline :: String -> String -> String
-mergeWithNewline x y
-    | x == "" && y == "" = ""
-    | otherwise = x ++ "\n" ++ y
+appendNewline :: (String, String) -> (String, String)
+appendNewline (a, b)
+              | b == "" = (a, b)
+              | otherwise = (a, b ++ "\n")
 
-mergeBlocks :: [(String, String)] -> M.Map String String
-mergeBlocks x = fromListWith mergeWithNewline x
+mergeBlocks :: [(String, String)] -> [(String, String)]
+mergeBlocks = M.assocs . fromListWith (++)
 
-main = interact (show . mergeBlocks . extractCodeblocks . decodeJSON )
+getBlocks :: Pandoc -> [(String, String)]
+getBlocks x = mergeBlocks $ Prelude.map appendNewline $ extractCodeblocks x
+
+writeBlocks :: (String, String) -> IO()
+writeBlocks (f, c) = writeFile f c
+
+main = do
+  contents <- getContents
+  let doc = decodeJSON contents
+      blocks = getBlocks doc
+  mapM_ writeBlocks blocks
