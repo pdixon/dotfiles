c067b710aaf712e7682d929f424d0085a2a8147c
Phillip DIXON <phil@dixon.gen.nz>
2012-03-05 22:42:43 +1300

    Option handling for tangle.

--- src/tangle.hs
+++ src/tangle.hs
@@ -1,3 +1,6 @@
+import System
+import System.Console.GetOpt
+
 import Text.Pandoc
 import Text.JSON.Generic
 import Data.Map as M hiding (lookup)
@@ -26,8 +29,46 @@ getBlocks x = mergeBlocks $ Prelude.map appendNewline $ extractCodeblocks x
 writeBlocks :: (String, String) -> IO()
 writeBlocks (f, c) = writeFile f c
 
+listFile :: (String, String) -> IO()
+listFile (f,_) = putStrLn f 
+
+data Options = Options {
+      optInput :: IO String,
+      optAction :: (String, String) -> IO()
+    }
+
+defaultOptions :: Options
+defaultOptions = Options {
+                   optInput = getContents,
+                   optAction = writeBlocks
+                 }
+
+options :: [OptDescr (Options -> IO Options)]
+options = [
+    Option ['V'] ["version"] (NoArg showVersion) "show version number",
+    Option ['i'] ["input"] (ReqArg readInput "FILE") "input file to read",
+    Option ['l'] ["list"] (NoArg setList) "list embedded files"
+  ]
+
+showVersion :: Options -> IO Options
+showVersion _ = do
+  putStrLn "tangle 0.1"
+  exitWith ExitSuccess
+
+readInput arg opt = return opt { optInput = readFile arg }
+setList opt = return opt { optAction = listFile }
+
+tangleOpts :: [String] -> IO Options
+tangleOpts argv =
+    case getOpt RequireOrder options argv of
+      (o,[],[] ) -> foldl (>>=) (return defaultOptions) o
+      (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = "Usage: tangle [OPTION...]"
+
 main = do
-  contents <- getContents
-  let doc = decodeJSON contents
-      blocks = getBlocks doc
-  mapM_ writeBlocks blocks
+  args <- getArgs
+  opts <- tangleOpts args
+  contents <- optInput opts
+  let blocks = getBlocks (decodeJSON contents)
+      act = optAction opts
+  mapM_ act blocks
