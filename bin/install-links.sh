96f9e08ad41ce1e64b27ad4ac3514a7d6ee08d6b
Phillip DIXON <phil@dixon.gen.nz>
2013-04-05 20:50:37 +1300

    Remove unused stuff from the links to install.

--- bin/install-links.sh
+++ bin/install-links.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-for item in bashrc gitconfig hgrc vim vimrc bash_profile inputrc pythonrc.py zshrc Xresources tmux.conf i3 dunstrc
+for item in bashrc gitconfig hgrc bash_profile inputrc pythonrc.py zshrc Xresources tmux.conf i3 dunstrc
 do
-	if [ -h ~/."$item" ]
-	then
-		rm ~/."$item"
-	fi
-	if [ -e ~/."$item" ]
-	then
-		mv ~/."$item"{,.old}
-	fi
-	ln -s {~/dotfiles/,~/.}"$item"
+    if [ -h ~/."$item" ]
+    then
+        rm ~/."$item"
+    fi
+    if [ -e ~/."$item" ]
+    then
+        mv ~/."$item"{,.old}
+    fi
+    ln -s {~/dotfiles/,~/.}"$item"
 done
