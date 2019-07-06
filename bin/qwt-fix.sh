0aab40bf687701555665602d35967e2605bdddc9
Phillip Dixon <phil@dixon.gen.nz>
2019-07-06 21:02:11 +1200

    Drop obsolete old helpers

--- bin/qwt-fix.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-destroot="/usr/local/"
-version="5.2.0"
-prefix="qwt-"${version}
-
-install_name_tool -id ${destroot}${prefix}/lib/libqwt.${version}.dylib ${destroot}${prefix}/lib/libqwt.${version}.dylib
-install_name_tool -change libqwt.5.dylib ${destroot}${prefix}/lib/libqwt.5.2.0.dylib /Developer/Applications/Qt/plugins/designer/libqwt_designer_plugin.dylib
--- src/brst.hs
+++
@@ -1,4 +0,0 @@
--- git branch
--- git log --no-walk --pretty=format:"%ct %cr %h %s" BRANCH_NAME
-
-
